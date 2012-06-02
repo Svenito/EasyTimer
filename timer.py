39e75b80d1ab2ec248cdc768beffb61313004b6d
Sven Steinbauer <sven@unlogic.co.uk>
2012-06-02 10:20:34 +0100

    Support config file to store settings

--- timer.py
+++ timer.py
@@ -2,6 +2,9 @@
 import sys
 from PyQt4 import QtGui, QtCore
 from ui.timerUI import Ui_Timer
+import datetime
+import ConfigParser
+import os
 
 class SetupUI(QtGui.QDialog):
     def __init__(self, parent):
@@ -10,9 +13,20 @@ class SetupUI(QtGui.QDialog):
         self.ui.setupUi(self)
         self.parent = parent
 
-        self.connect(self.ui.buttonBox, QtCore.SIGNAL("accepted()"), self.setValues)
+        self.connect(self.ui.buttonBox, QtCore.SIGNAL("accepted()"), self.setTimerValues)
+    
+    def setValues(self, time, msg):
+        time = str(datetime.timedelta(seconds=time/1000)).split(':')
+        hours = int(time[0])
+        minutes = int(time[1])
+        seconds = int(time[2])
 
-    def setValues(self):
+        self.ui.HourSpin.setValue(hours)
+        self.ui.MinuteSpin.setValue(minutes)
+        self.ui.SecondSpin.setValue(seconds)
+        self.ui.PopupEdit.setText(msg)
+
+    def setTimerValues(self):
         hours = self.ui.HourSpin.value()
         minutes = self.ui.MinuteSpin.value()
         seconds = self.ui.SecondSpin.value()
@@ -59,8 +73,42 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
         self.onIcon = QtGui.QIcon("res/TimerOn.png")
         self.offIcon = QtGui.QIcon("res/TimerOff.png")
 
+        self.loadConfig()
+
+    def loadConfig(self):
+        self.useConfig = True
+        configPath = os.path.expanduser(os.path.join("~", ".config", "easytimer"))
+        if not os.path.exists(configPath):
+            try:
+                os.makedirs(configPath)
+            except:
+                self.useConfig = False
+                return
+
+        self.configFile = os.path.join(configPath, "conf")
+
+        self.config = ConfigParser.ConfigParser()
+        opened_files = self.config.read(self.configFile)
+        if len(opened_files) == 0:
+            self.config.add_section("main")
+            try:
+                f = open(self.configFile, 'w+')
+                f.close()
+            except:
+                self.useConfig = False
+                pass
+        else:
+            self.timeout = self.config.getint("main", "seconds")
+            self.msg = self.config.get("main", "msg")
+
     def exit(self):
         sys.exit(0)
+
+    def error(self, msg):
+         QtGui.QMessageBox.warning(None,
+                    "Error",
+                    (msg),
+                    QtGui.QMessageBox.Ok)
     
     def startTimer(self):
         if self.timeout < 1:
@@ -91,14 +139,21 @@ class SystemTrayIcon(QtGui.QSystemTrayIcon):
     def setup(self):
         if self.setupDialog is None:
             self.setupDialog = SetupUI(self)
-            self.setupDialog.show()
-        else:
-            self.setupDialog.show()
+        
+        self.setupDialog.setValues(self.timeout, self.msg)
+        self.setupDialog.show()
+        self.setupDialog.raise_()
 
     def setValues(self, seconds, msg):
         self.timeout = seconds * 1000
         self.msg = msg
 
+        if self.useConfig:
+            self.config.set("main", "seconds", self.timeout)
+            self.config.set("main", "msg", self.msg)
+
+            with open(self.configFile, 'wb') as config_fh:
+                self.config.write(config_fh)
 
 def main():
     app = QtGui.QApplication(sys.argv)
